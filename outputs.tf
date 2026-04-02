48e79a7e368691c11b74bb166464f12afd15ab0c
saloni180 <saloni180@gmail.com>
2026-04-02 15:28:06 +0530

    add deleted file

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-//Include an output variables for the Storage Account ID.
-
-output "storage_account_id" {
-  value       = azurerm_storage_account.storage.id
-  description = "The storage account name"
-}
