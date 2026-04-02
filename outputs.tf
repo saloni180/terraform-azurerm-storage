a969ad696ca2d39f16edc21d84548a09ca7b7064
saloni180 <saloni180@gmail.com>
2026-04-02 15:35:48 +0530

    add output file

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+//Include an output variables for the Storage Account ID.
+
+output "storage_account_id" {
+  value       = azurerm_storage_account.storage.id
+  description = "The storage account id"
+}
